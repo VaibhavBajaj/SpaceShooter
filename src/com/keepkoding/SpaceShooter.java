2cfa4ab4bfa4e471bc2027f6c826394ea865b29a
David Akeley <dza724@gmail.com>
2017-04-22 00:35:30 -0700

    Added test exit buttons demonstrating basic interface for Buttons class.

--- src/com/keepkoding/SpaceShooter.java
+++ src/com/keepkoding/SpaceShooter.java
@@ -10,12 +10,12 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 import java.text.DecimalFormat;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.swing.JFrame;
@@ -31,8 +31,6 @@ public class SpaceShooter extends JPanel{
     
     private static AudioClip bgSound = MusicLoader.loadClip("hindiBgSound.wav");
     
-    static final AtomicBoolean gameResetRequested = new AtomicBoolean(false);
-    
     private static boolean gameOver, incSpeed, decSpeed, incAngle, decAngle;
     
     static PlayerShip playerShip;
@@ -46,9 +44,17 @@ public class SpaceShooter extends JPanel{
     private static long nextEnemySpawnTick, nextAsteroidSpawnTick;
     private static long ticksPerAsteroidSpawn, ticksPerEnemySpawn;
     
+    private static final Buttons testExitButtons;
+    
+    static {
+        testExitButtons = new Buttons();
+        BufferedImage buttonImage = ImageLoader.load("exit.png");
+        testExitButtons.add(new Button(1, buttonImage, 100, 200));
+        testExitButtons.add(new Button(2, buttonImage, 900, 130));
+    }
+    
     private SpaceShooter() {
         addKeyListener(new MyKeyListener());
-        addMouseListener(new Buttons());
         setFocusable(true);
     }
     
@@ -184,7 +190,7 @@ public class SpaceShooter extends JPanel{
         Graphics2D g2d = (Graphics2D) g;
         // Paint the background
         gameBoard.paintField(g2d);
-        if(!gameOver) {
+        if (!gameOver) {
             // Paint the player
             playerShip.paint(g2d);
         }
@@ -224,9 +230,10 @@ public class SpaceShooter extends JPanel{
             (SpaceShooter.screenHeight / 24) - 10
         );
         
-        // If game has ended, paint the gameOver sign
+        // If game has ended, paint the gameOver sign and exit buttons.
         if (gameOver) {
             gameBoard.paintGameOver(g2d);
+            testExitButtons.paint(g2d);
         }
     }
     
@@ -260,8 +267,9 @@ public class SpaceShooter extends JPanel{
             long lastTime = System.nanoTime();
             long NsPerFrame = 33333333;         // 30 fps is our target.
             
-            gameResetRequested.set(false);
-            while (!gameResetRequested.get()) {
+            boolean showingExitButton = false;
+            
+            while (true) {
                 long nextFrameTime = lastTime + NsPerFrame;
 
                 while (System.nanoTime() < nextFrameTime) {
@@ -278,6 +286,21 @@ public class SpaceShooter extends JPanel{
 
                 lastTime = nextFrameTime;
                 ++currentTick;
+                
+                if (gameOver) {
+                    if (!showingExitButton) {
+                        testExitButtons.listenToPanel(gamePanel);
+                        showingExitButton = true;
+                    }
+                    int buttonID = testExitButtons.getPushedButton();
+                    if (buttonID != Buttons.NONE) {
+                        testExitButtons.listenToPanel(null);
+                        showingExitButton = false;
+                        System.out.println("Pushed test button #" + buttonID);
+                        System.out.println("Points: " + points);
+                        break;
+                    }
+                }
             }
         }
     }
@@ -304,9 +327,6 @@ public class SpaceShooter extends JPanel{
                 case KeyEvent.VK_DOWN:
                     decSpeed = true;
                     break;
-                case KeyEvent.VK_ESCAPE:
-                    gameResetRequested.set(true);
-                    break;
             }
         }
 
@@ -330,9 +350,13 @@ public class SpaceShooter extends JPanel{
         }
     }
     
+    /*  Button class used for telling the Buttons class (see below)
+     *  information about a button's location and appearance (BufferedImage).
+     */
     private static class Button {
         final BufferedImage image;
         final int x, y, xSize, ySize, id;
+        final AffineTransform transform;
         
         Button(int id, BufferedImage image, int x, int y) {
             this.id = id;
@@ -341,6 +365,7 @@ public class SpaceShooter extends JPanel{
             this.y = y;
             this.xSize = image.getWidth();
             this.ySize = image.getHeight();
+            this.transform = AffineTransform.getTranslateInstance(x, y);
         }
         
         boolean clicked(int x, int y) {
@@ -351,12 +376,27 @@ public class SpaceShooter extends JPanel{
         }
     }
     
-    private static class Buttons implements MouseListener {
-        ArrayList<Button> buttonList = new ArrayList<Button>();
+    /** Class representing a group of buttons. Each  button  should  have  a
+     *  distinct,  nonzero  ID  associated  that will be used to communicate
+     *  which  button  was  pushed  to  the  user  of  the   class   through
+     *  getPushedButton,  which  returns 0 if no button was pushed. When the
+     *  class is initialized, it does not start listening for button pushes.
+     *  It  must  be told to listen to a panel using listenToPanel for mouse
+     *  events  that  will  be  translated  to  button  press  IDs.  Calling
+     *  listenToPanel  with  null  will  restore the Buttons instance to the
+     *  default state of not listening for any button presses. Call paint on
+     *  the  Graphics2D  object  associated  with  the  same  panel used for
+     *  listening to mouse events to paint the buttons.
+     */
+    private static class Buttons {
         static final int NONE = 0;
-        AtomicInteger pushedButton = new AtomicInteger(NONE);
         
-        void addButton(Button button) {
+        ArrayList<Button> buttonList = new ArrayList<Button>();
+        AtomicInteger pushedButton = new AtomicInteger(NONE);
+        JPanel listeningPanel = null;
+        Listener listener = new Listener();
+        
+        void add(Button button) {
             int size = buttonList.size();
             for (int i = 0; i < size; ++i) {
                 if (buttonList.get(i).id == button.id) {
@@ -373,44 +413,63 @@ public class SpaceShooter extends JPanel{
             return pushedButton.getAndSet(NONE);
         }
         
-        @Override
-        public void mousePressed(MouseEvent e) {
-            if (e.getButton() != MouseEvent.BUTTON1) {
-                return;
-            }
-            
-            int x = e.getX();
-            int y = e.getY();
-            
-            System.out.println(x + ", " + y);
-            
-            for (int i = buttonList.size() - 1; i != -1; --i) {
+        void paint(Graphics2D g2d) {
+            int size = buttonList.size();
+            for (int i = 0; i < size; ++i) {
                 Button b = buttonList.get(i);
-                if (b.clicked(x, y)) {
-                    pushedButton.set(b.id);
-                    break;
+                g2d.drawRenderedImage(b.image, b.transform);
+            }
+        }
+        
+        void listenToPanel(JPanel panel) {
+            if (listeningPanel != null) {
+                listeningPanel.removeMouseListener(listener);
+            }
+            if (panel != null) {
+                panel.addMouseListener(listener);
+            }
+            listeningPanel = panel;
+            pushedButton.set(NONE);
+        }
+        
+        private class Listener implements MouseListener { 
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() != MouseEvent.BUTTON1) {
+                    return;
+                }
+                
+                int x = e.getX();
+                int y = e.getY();
+                
+                for (int i = buttonList.size() - 1; i != -1; --i) {
+                    Button b = buttonList.get(i);
+                    if (b.clicked(x, y)) {
+                        pushedButton.set(b.id);
+                        break;
+                    }
                 }
             }
-        }
-        
-        @Override
-        public void mouseEntered(MouseEvent e) {
-        
-        }
-        
-        @Override
-        public void mouseExited(MouseEvent e) {
-        
-        }
-        
-        @Override
-        public void mouseReleased(MouseEvent e) {
-        
-        }
-        
-        @Override
-        public void mouseClicked(MouseEvent e) {
-        
+            
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            
+            }
+            
+            @Override
+            public void mouseExited(MouseEvent e) {
+            
+            }
+            
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            
+            }
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            
+            }
         }
     }
 }
